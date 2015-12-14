ef9d80802ae1370341462f64517382b8f0d04695
stevejamesson <steve@evendev.com>
2015-12-14 08:11:54 -0500

    initial commit

---
+++ src/Flash.php
@@ -0,0 +1,92 @@
+<?php 
+
+namespace Jevets\Kirby;
+
+use \s as Session;
+
+class Flash
+{
+    /**
+     * A Session key identifier
+     *
+     * @var constant
+     */
+    const SESSION_KEY = '_flash';
+
+    /**
+     * The singleton instance
+     *
+     * @var Jevets\Kirby\Flash
+     */
+    private static $instance;
+
+    /**
+     * Container for the flashed data
+     *
+     * @var array
+     */
+    private static $data;
+
+    /**
+     * Get the singleton instance
+     *
+     * @return Jevets\Kirby\Flash
+     */
+    public static function getInstance()
+    {
+        if (null === static::$instance)
+        {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * Instantiate the singleton
+     *
+     * @return void
+     */
+    private function __construct()
+    {
+        static::$data = Session::get(self::SESSION_KEY, []);
+
+        Session::remove(self::SESSION_KEY);
+    }
+
+    /**
+     * Set flash data
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public static function set($key, $value)
+    {
+        static::$data[$key] = $value;
+
+        Session::set(self::SESSION_KEY, static::$data);
+    }
+
+    /**
+     * Get an item from the flash data by key
+     *
+     * @param  string  $key
+     * @param  mixed  $default value to return if flash key doesn't exist
+     * @return  mixed  $value or null
+     */
+    public static function get($key, $default = '')
+    {
+        return isset(self::$data[$key]) ? self::$data[$key] : null;
+    }
+
+    /**
+     * Get all data from the flash
+     *
+     * @return  array
+     */
+    public static function all()
+    {
+        return self::$data;
+    }
+}
